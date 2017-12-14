65a6ce0cd087e0e2c2070c5a1ecdc05a5ea72b37
Tobias-Ternent <tobias@ebi.ac.uk>
2017-12-14 11:20:25 +0000

    Cleaned code, whitespace, logic, JavaDoc, comments.

--- src/test/java/uk/ac/ebi/pride/utilities/contaminant/MatchContaminantTest.java
+++ src/test/java/uk/ac/ebi/pride/utilities/contaminant/MatchContaminantTest.java
@@ -6,26 +6,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * This code is licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * ==Overview==
- * <p>
- * This class
- * <p>
- * Created by ypriverol (ypriverol@gmail.com) on 24/11/2017.
+ * Tests reading in and matching from the contaminates FASTA file.
  */
 public class MatchContaminantTest {
 
+  /**
+   * Checks if a contaminant peptide is present, and if a non-contaminant peptide is not present.
+   * @throws Exception assert failures from incorrect contaminant searching results.
+   */
     @Test
     public void isPeptideContaminant() throws Exception {
-
         String peptide = "FNTGVQEGAKALYANLEPKAEQYAVI";
         Assert.assertTrue(MatchContaminant.instance().isPeptideContaminant(peptide));
-
         peptide = "KKKKKKKKKKKKKKRRRR";
         Assert.assertTrue(!MatchContaminant.instance().isPeptideContaminant(peptide));
     }
